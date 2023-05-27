b253babcea98afae020381a35ca858b453d02b58
Novac Sviatlana <s.novac@andersenlab.com>
2023-05-27 14:44:28 +0300

    clean all unused code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import BrickCatAPP from './BrickCatAPP/BrickCatAPP';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-  {/* <App/> */}
 root.render(
     <BrickCatAPP/>
 );
